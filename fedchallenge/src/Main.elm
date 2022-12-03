0a2cbe37cab90ab82557d3da7e256992d8f3524e
Gaurav M <emailgauravmishra@gmail.com>
2022-12-03 20:34:42 +0530

    Initial Commit

---
+++ fedchallenge/src/Main.elm
@@ -0,0 +1,91 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    Element.layout  [width fill
+                    , height fill
+                    , Background.color <| rgb255 224 255 255
+                    ]
+     <| Element.column [width fill, Element.spacing 80] 
+            [ row [width fill
+                    , Element.centerX] 
+                    [   row   [ width fill
+                            , spacing 10
+                            , Border.rounded 20
+                            , Background.color (rgb255 68 204 238)
+                            , inFront <| image  [ Border.rounded 10
+                                                , padding 30
+                                                ]
+                                                { src = "/logo.jpg"
+                                                , description = "Talenteca"
+                                                }
+                            ] [ el [Element.centerX, Font.bold] <| (text " Frontend Developer Challenge " )]
+                    
+                    
+                   
+                    ]
+                
+            , el [Element.centerX] (text "Elm web page accessing a JSON API " )
+            , row [
+                  width fill, 
+                  spacing 20, 
+                  padding 20,
+                  Background.color <| rgb255 64 224 208]
+                  [text "here will go the row"]
+            ]
+{- <|
+    el
+        [ Background.color (rgb255 64 224 208)
+        , Font.color (rgb255 255 255 255)
+        , Border.rounded 10
+        , padding 30
+        ]
+        (text "stylish!")
+-}
+---- PROGRAM ----
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
